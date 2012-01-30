667d6789a5c6ea16f819bdcb4ee223836b257c51
Sarah Becker <becker@fias.uni-frankfurt.de>
2012-01-30 10:12:08 +0100

    transition to sparse matrices: final version. speedup by a factor of a little more than 2\!

--- zdcpf.py
+++ zdcpf.py
@@ -226,7 +226,6 @@ def AtoKh(N,pathadmat='./settings/admat.txt'):
     K_values=[]
     K_column_indices=[]
     K_row_indices=[]
-    Kold=np.zeros((len(Ad),L))
     h=np.zeros(L*2)
     h=np.append(h,np.zeros(3*len(Ad)))
     L=0
@@ -239,17 +238,15 @@ def AtoKh(N,pathadmat='./settings/admat.txt'):
                     K_values.extend([1,-1])
                     K_column_indices.extend([L,L])
                     K_row_indices.extend([j,i])
-                    Kold[j,L]=1
-                    Kold[i,L]=-1
                     h[2*L]=Ad[i,j]
                     h[2*L+1]=Ad[j,i]
                     if L>0: listFlows.append([str(N[j-1].label)+" to " +str(N[i-1].label), L-1])
                     L+=1
     K=spmatrix(K_values,K_row_indices,K_column_indices)
-    return K,Kold,h, listFlows               
+    return K,h, listFlows               
 
 def generatemat(N,admat='admat.txt',b=None,path='./settings/',copper=0,h0=None):
-    K,Kold,h, listFlows=AtoKh(N,path+admat)
+    K,h, listFlows=AtoKh(N,path+admat)
     if h0 != None: 
         h[2:88]=h0
     if b != None:
@@ -262,19 +259,14 @@ def generatemat(N,admat='admat.txt',b=None,path='./settings/',copper=0,h0=None):
     P1=spmatrix(1,range(Nlinks),range(Nlinks))
     P2=spmatrix(1e-6,range(2*Nnodes),range(2*Nnodes)) # and the bal/cur part has dif. coeffs
     P= spdiag([P1,P2])
-    P1old=np.eye(Nlinks+2*Nnodes) 
-    Pold=np.concatenate((P1old[:Nlinks],P1old[-2*Nnodes:]*1e-6))  # and the bal/cur part has dif. coeffs
     # Then we make the q vector, whose values will be changed all the time
     q=np.zeros(Nlinks+2*Nnodes)  # q has the same size and structure as the solution 'x'
     # Then we build the equality constraint matrix A
     # The stucture is more or less [ K | -I | I ]
     A1=spmatrix(1.,range(Nnodes),range(Nnodes))
     A=sparse([[K],[-A1],[A1]])
-    A1old=np.concatenate((matrix(K),-np.eye(Nnodes)),axis=1)
-    Aold=np.concatenate((A1old,np.eye(Nnodes)),axis=1)
     # See documentation for why first row is cut
     A=A[1:,:]
-    Aold=np.delete(Aold,np.s_[0],axis=0)
     # b vector will be defined by the mismatches, in MAIN
     # Finally, the inequality matrix and vector, G and h.
     # Refer to doc to understand what the hell I'm doing, as I build
@@ -287,27 +279,16 @@ def generatemat(N,admat='admat.txt',b=None,path='./settings/',copper=0,h0=None):
       G1_col_ind.extend([i,i])
     G1_row_ind=range(2*Nlinks)
     G1=spmatrix(G1_values,G1_row_ind,G1_col_ind)
-    G1old=np.eye(Nlinks)
-    for i in range(Nlinks-1):
-        i+=i
-        G1old=np.insert(G1old,i+1,-G1old[i],axis=0)
-    G1old=np.concatenate((G1old,-G1old[-1:]))
     # to model copper plate, we forget about the effect of G matrix on the flows
     if copper == 1:
         G1=spmatrix([1,-1],[0,1],[0,0],(2*Nlinks,Nlinks))
-        G1old*=0
-        G1old[0,0]=1
-        G1old[1,0]=-1
     # G1 is ready, now we make G2
     G2=spmatrix([],[],[],(2*Nlinks,2*Nnodes))
-    G2old=np.zeros((2*Nlinks,2*Nnodes))
     # G3 is built as [ 0 | -I | 0 ]
     G3=spmatrix(-1.,range(Nnodes),range(Nnodes))
     G3_hlp1=spmatrix([],[],[],(Nnodes,Nlinks))
     G3_hlp2=spmatrix([],[],[],(Nnodes,Nnodes))
     G3=sparse([[G3_hlp1],[G3],[G3_hlp2]])
-    g3=np.concatenate((np.zeros((Nnodes,Nlinks)),-np.eye(Nnodes)),axis=1)
-    G3old=np.concatenate((g3,np.zeros((Nnodes,Nnodes))),axis=1)
     G4_values=[]
     G4_row_ind=[]
     G4_col_ind=[]
@@ -316,21 +297,11 @@ def generatemat(N,admat='admat.txt',b=None,path='./settings/',copper=0,h0=None):
       G4_col_ind.extend([i,i])
     G4_row_ind=range(2*(Nnodes))
     G4=spmatrix(G4_values,G4_row_ind,G4_col_ind)
-    g4=np.eye(Nnodes)
-    G4old=g4
-    for i in range(Nnodes-1):
-        i+=i
-        G4old=np.insert(G4old,i+1,-G4old[i],axis=0)
-    G4old=np.concatenate((G4old,-G4old[-1:]))
     G5=spmatrix([],[],[],(2*(Nnodes),Nlinks+Nnodes))
     G5=sparse([[G5],[G4]])
-    G5old=np.concatenate((np.zeros((2*Nnodes,Nlinks+Nnodes)),G4old),axis=1)
     G=sparse([[G1],[G2]])
     G=sparse([G,G3,G5])
-    Gold=np.concatenate((G1old,G2old),axis=1)
-    Gold=np.concatenate((Gold,G3old))
-    Gold=np.concatenate((Gold,G5old))
-    return P,Pold,q,G,Gold,h,A,Aold,K,Kold, listFlows
+    return P,q,G,h,A,K, listFlows
 
 def runtimeseries(N,F,P,q,G,h,A,coop,lapse):
     if lapse==None:
